8ae6ccbbd2ec9ae62b4528f6565d26a22968f1a0
ragul66 <114981373+ragul66@users.noreply.github.com>
2023-09-22 21:54:37 +0530

    problem 3 started

--- problem3.py
+++
@@ -1,6 +0,0 @@
-"""1.SELECT FROM name where city="MAA";
-2.SELECT COUNT(city)
-FROM student table
-WHERE city="TRI";
-3.
-4.SELECT AVG(age) FROM student table; """
---
+++ problem4.py
@@ -0,0 +1,6 @@
+"""1.SELECT FROM name where city="MAA";
+2.SELECT COUNT(city)
+FROM student table
+WHERE city="TRI";
+3.SELECT COUNT()
+4.SELECT AVG(age) FROM student table; """
